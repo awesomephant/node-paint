b6ed1e8a992092e094b947059982b849d9c66e04
Max Kohler <max@koehler-kn.de>
2019-08-23 11:53:28 +0100

    Updates

---
+++ precache-manifest.a2d6d5fdf16bdb2616a4615cf9e0bdcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eee995175b3f80ff9a6fef10d039bb5",
+    "url": "/node-paint/index.html"
+  },
+  {
+    "revision": "be76c3a0a2e161dd641a",
+    "url": "/node-paint/static/css/main.7f1af4cc.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/node-paint/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "be76c3a0a2e161dd641a",
+    "url": "/node-paint/static/js/main.88f13fb7.chunk.js"
+  },
+  {
+    "revision": "38d43e3a7e01ec57cd3b",
+    "url": "/node-paint/static/js/runtime~main.d6ede3dc.js"
+  }
+]);
